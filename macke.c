040f07d8eb7c58ce40d0e9ffde62209061babaf8
unknown <niklas.danell@hotmail.com>
2017-11-16 12:58:30 +0100

    printf removed

--- macke.c
+++ macke.c
@@ -49,15 +49,15 @@ int main()
         printf("\n");
         int val;
 
-        printf("Välj vilka storheter du vill beräkna:\n");
-        printf("Välj 1 för: OHMS LAG\n");
-        printf("Välj 2 för: Rtot\n");
-        printf("Välj 3 för: EFFEKTLAGEN ENKEL\n");
-        printf("Välj 4 för: SKENBAR EFFEKT ENFAS\n");
-        printf("Välj 5 för: AKTIV EFFEKT/MEDELEFFEKT ENFAS\n");
-        printf("Välj 6 för: SKENBAR EFFEKT 3-FAS\n");
-        printf("Välj 7 för: AKTIV EFFEKT 3-FAS\n");
-        printf("Välj 0 för: FÖR ATT AVSLUTA\n");
+        printf("Välj vilka storheter du vill beräkna:\n"
+        "Välj 1 för: OHMS LAG\n"
+        "Välj 2 för: Rtot\n"
+        "Välj 3 för: EFFEKTLAGEN ENKEL\n"
+        "Välj 4 för: SKENBAR EFFEKT ENFAS\n"
+        "Välj 5 för: AKTIV EFFEKT/MEDELEFFEKT ENFAS\n"
+        "Välj 6 för: SKENBAR EFFEKT 3-FAS\n"
+        "Välj 7 för: AKTIV EFFEKT 3-FAS\n"
+        "Välj 0 för: FÖR ATT AVSLUTA\n");
 
         scanf("%d", &val);
 
